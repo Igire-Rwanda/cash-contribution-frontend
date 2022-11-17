91381de0608012b6b735f26c601a100631a4ef36
Rebecca <nzamuyerebecca@gmail.com>
2022-11-17 17:01:36 +0200

    signup form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,17 @@ import React from 'react'
 import GetSarted from '../components/GetStarted'
 import HeloSection from '../components/HeloSection'
 
+
 const Home = () => {
   return (
     <>
       <HeloSection />
       <GetSarted />
     </>
+  
+       
+       
+    
   )
 }
 
